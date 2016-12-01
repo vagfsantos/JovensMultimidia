1e7eee6450e83ee727156d180be038d52a40e5d8
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-12-01 00:20:59 -0200

    tasks and workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,85 @@ module.exports = function(grunt){
 
 	grunt.initConfig({
 
-		watch: {
-		  src: {
-		    files: ['css/**/*.scss'],
-		    tasks: ['sass'],
-		    options: {
-		      spawn: false,
-		    },
-		  },
+		clean: {
+		  	target: {
+		    	src: ['build']
+		  	}
 		},
 
-		// compilar SASS
 		sass: {
-			dist:{
-				files: { 'css/main.css':'css/sass/main.scss' }
+			target:{
+				files: { 
+					'build/css/main.css' : '_src/css/scss/main.scss'
+				}
+			}
+		},
+
+		uglify: {
+		    target: {
+		      	files: [{
+		          	expand: true,
+		          	cwd: '_src/js',
+		          	src: '*.js',
+		          	dest: 'build/js',
+		          	ext: '.min.js'
+		      	}]
+		    }
+		},
+
+		concat: {
+		    missing: {
+		      	src: ['_src/js/modules/**/*.js'],
+		      	dest: '_src/js/main.js'
+		    }
+		},
+
+		cssmin: {
+			target: {
+			    files: [{
+			      	expand: true,
+			      	cwd: 'build/css',
+			      	src: ['*.css'],
+			      	dest: 'build/css',
+			      	ext: '.min.css'
+			    }]
+			}
+		},
+
+		image: {
+			dynamic: {
+		        files: [{
+		          	expand: true,
+		          	cwd: '_src/img',
+		          	src: ['**/*.{png,jpg,gif,svg}'],
+		          	dest: 'build/img'
+		        }]
+			}
+		},
+
+		watch: {
+			js:{
+				files: ['_src/js/**/*.js'],
+			    tasks: ['concat', 'uglify'],
+			    options: {
+					event: ['added', 'changed', 'deleted']
+				}
+			},
+
+			css:{
+				files: ['_src/css/scss/**/*.scss'],
+			    tasks: ['sass', 'cssmin'],
+			    options: {
+					event: ['added', 'changed', 'deleted']
+				}
+			},
+
+			img:{
+				files: ['_src/img/**/*.{png,jpg,gif,svg}'],
+			    tasks: ['image'],
+			    options: {
+					event: ['added', 'changed', 'deleted']
+				}
 			}
 		}
 
@@ -23,7 +88,13 @@ module.exports = function(grunt){
 
 
 	grunt.loadNpmTasks("grunt-contrib-sass");
+	grunt.loadNpmTasks("grunt-image");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-watch");
 
-	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('default', ['clean', 'image', 'sass', 'cssmin', 'concat', 'uglify', 'watch']);
 }
--- _src/js/main.js
+++ _src/js/main.js
@@ -1,12 +1,2 @@
-function jmSearch(){
-	var btn = document.querySelector("#jm_btn_seach");
-	var campo = document.querySelector("#jm_campo_busca");
-
-	function toggleCampo(){
-		campo.classList.toggle("active");
-	}
-
-	btn.addEventListener("click", toggleCampo);
-}
-
-window.onload = jmSearch;
+var b = 'teste';
+var a = 'teste';
